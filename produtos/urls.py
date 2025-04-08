219c6c0841bbab5826796b466cad993f71a9ac70
Marcos Daniel <marcosdaniel.developer@hotmail.com>
2025-04-08 13:51:44 -0300

    Atibuindo um formulário para comprar

--- produtos/urls.py
+++ produtos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('ver-produto/', views.ver_produto, name='ver_produto')
+    path("ver-produto/", views.ver_produto, name="ver_produto"),
+    path("comprar-produto/", views.comprar_produto, name="comprar_produto"),
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def ver_produto(request):
-  return render(request,'ver.html',{'nome': 'notebook', 'preco': 5000.85})
+    return render(request, "ver.html", {"nome": "notebook", "preco": 5000.85})
+
+
+def comprar_produto(request):
+    return HttpResponse("Fui chamado")
