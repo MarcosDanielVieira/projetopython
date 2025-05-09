78d4176753bd694cd901e63430e83fe1f5f5b436
Marcos Daniel <marcosdaniel.developer@hotmail.com>
2025-05-09 14:48:24 -0300

    Atualização do projeto base

---
+++ config/ckeditor_5.py
@@ -0,0 +1,18 @@
+
+CKEDITOR_5_CONFIGS = {
+  "default": {
+    "toolbar": [
+      "heading",
+      "|",
+      "bold",
+      "italic",
+      "link",
+      "bulletedList",
+      "numberedList",
+      "|",
+      "undo",
+      "redo",
+    ],
+    "language": "pt-br",
+  }
+}
